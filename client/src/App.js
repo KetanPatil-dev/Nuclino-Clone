83af379c26e23319858349ec41484a889c12649d
ketan <mail.ketan027@gmail.com>
2025-05-31 12:20:01 +0530

    Implemented rooms for each document

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import TextEditor from "./TextEditor";
-
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
 
 function App() {
   return (
-    <div className="App">
-      <TextEditor/>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigate to={`/documents/${uuidV4()}`}/>} />
+      <Route path="/documents/:id" element={<TextEditor />} />
+    </Routes>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -6,7 +6,13 @@ const io = require("socket.io")(3002, {
 });
 
 io.on("connection", (socket) => {
+  socket.on("get-document",documentId=>{
+    const data=""
+    socket.join(documentId)
+    socket.emit("load-document",data)
+  
   socket.on("send-changes", (delta) => {
-  socket.broadcast.emit("receive-changes",delta)
+  socket.broadcast.to(documentId).emit("receive-changes",delta)
   });
 });
+})
